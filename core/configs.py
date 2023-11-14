b17e2d0a47ccddd5b6bf11ee2c3d8a8175c37417
ivancley <ivancley@hotmail.com>
2023-11-14 10:30:41 -0300

    Fast-API

---
+++ core/configs.py
@@ -0,0 +1,18 @@
+from typing import List
+from pydantic import BaseSettings, AnyHttpUrl
+from sqlalchemy.ext.declarative import declarative_base
+
+class Settings(BaseSettings):
+    '''
+    Configurações gerais do ambiente 
+    ''' 
+    API_V1_STR: str = "/api/v1"
+    DB_URL: str = "postgresql+asyncpg://admin:admim@localhost:5432/faculdade"
+    DBBaseModel = declarative_base()
+    
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
+    
